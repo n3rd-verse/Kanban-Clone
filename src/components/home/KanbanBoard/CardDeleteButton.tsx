1d835daec7547b343519033336528cef774382a6
jrjhjeje12 <jrhee2021@gmail.com>
2025-02-23 00:20:43 -0500

    Change Trash icon

--- src/components/home/KanbanBoard/CardDeleteButton.tsx
+++ src/components/home/KanbanBoard/CardDeleteButton.tsx
@@ -1,4 +1,4 @@
-import { Trash2 } from "lucide-react";
+import { LuTrash2 } from "react-icons/lu";
 
 interface DeleteButtonProps {
     onClick: () => void;
@@ -12,7 +12,7 @@ export function CardDeleteButton({ onClick, className }: DeleteButtonProps) {
             className={`w-10 h-10 flex items-center justify-center text-gray-500 hover:text-red-500 rounded-lg hover:bg-gray-100 transition-colors ${className}`}
             onClick={onClick}
         >
-            <Trash2 size={28} />
+            <LuTrash2 size={28} />
         </button>
     );
 }
