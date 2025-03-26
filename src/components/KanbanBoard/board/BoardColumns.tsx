b5ba707e0a3967b9f9977c8f7f95f975a52670c2
jrjhjeje12 <jrhee2021@gmail.com>
2025-03-25 22:03:38 -0400

    Update tests

--- src/components/KanbanBoard/board/BoardColumns.tsx
+++ src/components/KanbanBoard/board/BoardColumns.tsx
@@ -11,7 +11,10 @@ interface BoardColumnsProps {
 
 export function TaskColumns({ maxVisibleTasks, width }: BoardColumnsProps) {
     return (
-        <div className="content-start gap-2.5 grid grid-cols-4">
+        <div
+            className="content-start gap-2.5 grid grid-cols-4"
+            data-testid="task-columns"
+        >
             {STATUS_CONFIG.map(({ id }) => (
                 <QueryErrorBoundary key={id}>
                     <Suspense fallback={<ColumnSkeleton />}>
